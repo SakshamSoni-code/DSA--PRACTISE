259cf25f096162891f27b7eb84181995efb9140e
Saksham Soni <63503820+SakshamSoni-code@users.noreply.github.com>
2021-08-15 18:23:17 +0530

    Codeforces-C

---
+++ MATHS/C-sum of cubes.cpp
@@ -0,0 +1,93 @@
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*---------------------------------*//******GIVE YOUR BEST AND FORGET THE REST*****/
+
+
+
+//----------SAKSHAM SONI---------
+#include<bits/stdc++.h>
+
+
+#define fast       ios::sync_with_stdio(0); cin.tie(0)
+#define testcase    ll t;cin>>t;while(t--)
+#define fw(i,a,b)   for(int i=a;i<b;i++)
+#define fw1(i,a,b)  for(int i=a;i<=b;i++)
+#define bw(i,a,b)   for(i=a;i>b;i--)
+#define bw1(i,a,b)  for(i=a;i>=b;i--)
+#define pb          push_back
+#define vec         vector<ll>
+#define pll       pair<ll,ll>
+#define mll         map<ll,ll>
+#define F           first
+#define S           second
+#define lcm(a,b)    (a*b)/(__gcd(a,b))
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define prt(x)      cout<<x
+#define prts(x)     cout<<x<<" "
+#define prtl(x)     cout<<x<<endl
+#define prtnl()     cout<<"\n"
+#define endl        "\n"
+#define ll long long
+
+const ll N = 1000000000000; //10^12
+
+using namespace std;
+
+/*
+int gcdx( ll a, ll b)
+         {
+          if(b==0)return a%b;
+          else return gcdx(b,a%b);
+         }
+*/
+unordered_set<ll> cubes;
+
+void precal()
+{
+    for(ll i=1;i*i*i<=N;i++)
+    {
+        cubes.insert(i*i*i);
+    }
+}
+void solve()
+{
+    ll x;
+    cin>>x;
+    
+    for(ll i=1;i*i*i<=x;i++)
+    {
+        if(cubes.count(x-i*i*i))
+        {
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+}
+
+int32_t main() {
+    
+    fast;
+    precal();
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        solve();
+    }
+
+
+
+  return 0;
+}
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
